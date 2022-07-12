03decdd725941c2d1e793fe4f844f3f464eda8c1
joancesar <joancesar@hotmail.com>
2022-07-12 07:40:28 +0200

    Dependencias firebase
    Se añaden las librerias angular/fire y firebase para la conexión con Firestore Database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyClrteHsinYHDDy68h2GvqQ01RYzDAIwjM",
+    authDomain: "magister-eb82d.firebaseapp.com",
+    projectId: "magister-eb82d",
+    storageBucket: "magister-eb82d.appspot.com",
+    messagingSenderId: "481940127933",
+    appId: "1:481940127933:web:09e252c8c59e060848f7d4"
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
